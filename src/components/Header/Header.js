e0ebc7776e2ea47bbb6ce9eb6e29d7ff582be8f8
gezahegnw <gezahegnw@gmail.com>
2019-01-13 00:36:59 -0600

    push to heroku

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.css";
 
-const Header = props => (
+const Header = () => (
     <div className="header">
       <h1>Clicky Game</h1>
       <h2>Click on an image to earn point, but don not click on any more than once!</h2>
